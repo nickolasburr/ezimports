e40da117b411f889f41ed89f506aa98f57d675e3
Nickolas Burr <nickolasburr@gmail.com>
2019-03-07 22:27:24 -0500

    Add Imports class, update functions.php

---
+++ src/Imports.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace NickolasBurr\EzImports;
+
+class Imports
+{
+    /**
+     * Get imports.json file contents.
+     *
+     * @param string $path
+     * @return string
+     */
+    public static function getFile($path)
+    {
+    }
+}
