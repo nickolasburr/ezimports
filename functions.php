549595a6eb1c4518840700cb90c10410690b05c3
Nickolas Burr <nickolasburr@gmail.com>
2019-03-13 12:07:58 -0400

    Add type hints to Config methods, refactor functions.php

--- functions.php
+++ functions.php
@@ -8,28 +8,29 @@ if (!function_exists('include_imports')) {
     /**
      * Register PHP imports at runtime.
      *
-     * @param string $package The module name in 'vendor/package' format.
-     * @param string $class
+     * @param string $class FQCN of class/interface/trait/etc.
+     * @param string $module The module name in 'vendor/module' format.
      * @return void
      */
-    function include_imports($package, $class) {
-        /* Trim any leading/trailing slashes from package. */
-        $package = trim($package, '/');
-
+    function include_imports($class, $module) {
         /* Trim any leading/trailing backslashes from class. */
         $class = trim($class, '\\');
 
         /** @var string $namespace */
         $namespace = Config::getNamespaceFromFqcn($class);
 
+        /* Trim any leading/trailing slashes from module. */
+        $module = trim($module, '/');
+
         /** @var array $imports */
-        $imports = Config::getClassImports($package, $class);
+        $imports = Config::getClassImports($class, $module);
 
         foreach ($imports as $import) {
             /** @var string $use */
             $use = $import['use'] ?? null;
 
             if ($use === null) {
+                /** @todo: Output better exception message. */
                 throw new \Exception('No import path was specified.');
             }
 
